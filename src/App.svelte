a9f474dd6c391e93c4b6ff4fb0d704c5ebf847e8
Druid45ra <radu_vanca@live.com>
2025-02-03 17:03:12 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  let title = "Salut! Proiectul meu Svelte";
+</script>
+
+<main>
+  <h1>{title}</h1>
+  <p>Bun venit la aplicația mea cu Svelte 🚀</p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 2rem;
+  }
+  h1 {
+    color: #ff3e00;
+  }
+</style>
